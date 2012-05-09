86c58e5ab7b0f91f5fc880a8172cf23b22c04c02
Andrew <Spyder045@gmail.com>
2012-05-09 15:20:27 -0400

    cleaning up left over vb files

---
+++ TitheProgram/TitheProgram/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TitheProgram
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yearlyByMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yearlyTotalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(471, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newMemberToolStripMenuItem,
+            this.newRecordToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newMemberToolStripMenuItem
+            // 
+            this.newMemberToolStripMenuItem.Name = "newMemberToolStripMenuItem";
+            this.newMemberToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.newMemberToolStripMenuItem.Text = "&New Member";
+            this.newMemberToolStripMenuItem.Click += new System.EventHandler(this.newMemberToolStripMenuItem_Click);
+            // 
+            // newRecordToolStripMenuItem
+            // 
+            this.newRecordToolStripMenuItem.Name = "newRecordToolStripMenuItem";
+            this.newRecordToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.newRecordToolStripMenuItem.Text = "&New Record";
+            this.newRecordToolStripMenuItem.Click += new System.EventHandler(this.newRecordToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yearlyByMemberToolStripMenuItem,
+            this.yearlyTotalToolStripMenuItem});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.exitToolStripMenuItem.Text = "&Record";
+            // 
+            // yearlyByMemberToolStripMenuItem
+            // 
+            this.yearlyByMemberToolStripMenuItem.Name = "yearlyByMemberToolStripMenuItem";
+            this.yearlyByMemberToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.yearlyByMemberToolStripMenuItem.Text = "&Yearly by member";
+            // 
+            // yearlyTotalToolStripMenuItem
+            // 
+            this.yearlyTotalToolStripMenuItem.Name = "yearlyTotalToolStripMenuItem";
+            this.yearlyTotalToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.yearlyTotalToolStripMenuItem.Text = "&Yearly Total";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem1.Text = "&Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(471, 120);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yearlyByMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yearlyTotalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+    }
+}
+
