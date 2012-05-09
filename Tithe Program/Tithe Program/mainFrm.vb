75f3b56e571b0717bb525b509bc72614f18aab1e
Andrew <Spyder045@gmail.com>
2012-05-09 15:19:32 -0400

    Initial Build /Earlier build in vb

--- Tithe Program/Tithe Program/mainFrm.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class mainFrm
-
-    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
-        'Close the form
-        Me.Close()
-    End Sub
-End Class
